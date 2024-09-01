4a028cdd52eaade621895b00dd0e2ae397c88820
R-Dhanush <dhanu72004@gmail.com>
2024-09-01 21:09:57 +0530

    error handling

--- script.js
+++ script.js
@@ -5,44 +5,69 @@ const searchbtn = document.querySelector(".btn");
 const weathericon = document.querySelector("#weather_icon");
 
 async function fetchweather(city) {
-  const response = await fetch(apiurl + city + `&appid=${apikey}`);
-  var data = await response.json();
+  try {
+    const response = await fetch(apiurl + city + `&appid=${apikey}`);
+    if (!response.ok) {
+      throw new Error("City not found or network error occurred");
+    }
 
-  document.querySelector("#location_name").innerHTML = data.name;
+    const data = await response.json();
 
-  document.querySelector("#temp").innerHTML = data.main.temp;
-  document.querySelector("#mintemp").innerHTML = data.main.temp_min;
-  document.querySelector("#maxtemp").innerHTML = data.main.temp_max;
+    if (!data.main || !data.weather) {
+      throw new Error("Unexpected response format from API");
+    }
 
-  document.querySelector("#humidity").innerHTML = data.main.humidity;
-  document.querySelector("#dewpoint").innerHTML = (data.main.temp - (100 - data.main.humidity) / 5).toFixed(1);
-  document.querySelector("#pressure").innerHTML = data.main.pressure;
+    document.querySelector("#location_name").innerHTML = data.name;
 
-  document.querySelector("#windspeed").innerHTML = data.wind.speed;
-  document.querySelector("#winddirection").innerHTML = data.wind.deg;
+    document.querySelector("#temp").innerHTML = data.main.temp;
+    document.querySelector("#mintemp").innerHTML = data.main.temp_min;
+    document.querySelector("#maxtemp").innerHTML = data.main.temp_max;
 
-  if(data.weather[0].main == "Clear"){
-    weathericon.src = "img/clear.png";
-  }
-  else if(data.weather[0].main == "Clouds"){
-    weathericon.src = "img/clouds.png";
-  }
-  else if(data.weather[0].main == "Drizzle"){
-    weathericon.src = "img/drizzle.png";
-  }
-  else if(data.weather[0].main == "Mist"){
-    weathericon.src = "img/mist.png";
-  }
-  else if(data.weather[0].main == "Rain"){
-    weathericon.src = "img/rain.png";
-  }
-  else if(data.weather[0].main == "Snow"){
-    weathericon.src = "img/snow.png";
-  }
+    document.querySelector("#humidity").innerHTML = data.main.humidity;
+    document.querySelector("#dewpoint").innerHTML = (data.main.temp - (100 - data.main.humidity) / 5).toFixed(1);
+    document.querySelector("#pressure").innerHTML = data.main.pressure;
 
+    document.querySelector("#windspeed").innerHTML = data.wind.speed;
+    document.querySelector("#winddirection").innerHTML = data.wind.deg;
+
+    if(data.weather[0].main == "Clear"){
+      weathericon.src = "img/clear.png";
+    }
+    else if(data.weather[0].main == "Clouds"){
+      weathericon.src = "img/clouds.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+      weathericon.src = "img/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+      weathericon.src = "img/mist.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+      weathericon.src = "img/rain.png";
+    }
+    else if(data.weather[0].main == "Snow"){
+      weathericon.src = "img/snow.png";
+    }
+  } catch (error) {
+    alert("Please enter a valid city");
+    console.error("Error fetching weather data:", error);
+    document.querySelector("#temp").innerHTML = "N/A";
+    document.querySelector("#mintemp").innerHTML = "N/A";
+    document.querySelector("#maxtemp").innerHTML = "N/A";
+    document.querySelector("#humidity").innerHTML = "N/A";
+    document.querySelector("#dewpoint").innerHTML = "N/A";
+    document.querySelector("#pressure").innerHTML = "N/A";
+    document.querySelector("#windspeed").innerHTML = "N/A";
+    document.querySelector("#winddirection").innerHTML = "N/A";
+    weathericon.src = "";
+  }
 }
 
-searchbtn.addEventListener("click", ()=>{
-  fetchweather(searchcity.value);
-  console.log(searchcity.value);
-})
+searchbtn.addEventListener("click", () => {
+  const city = searchcity.value.trim();
+  if (city) {
+    fetchweather(city);
+  } else {
+    alert("Please enter a city name");
+  }
+});
